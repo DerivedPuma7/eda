1adac4d1b8baed39c0334e73a3bb5458cff40e3b
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-07-15 10:26:42 -0300

    test: prepare dispatcher test suite

--- pkg/events/event_dispatcher_test_mocks.go
+++ pkg/events/event_dispatcher_test_mocks.go
@@ -23,7 +23,9 @@ func (e *TestEvent) GetDatetime() time.Time {
   return time.Now()
 }
 
-type TestEventHandler struct{}
+type TestEventHandler struct{
+  ID int
+}
 
 func (h *TestEventHandler) Handle(event EventInterface) {}
 
@@ -39,9 +41,15 @@ type EventDispatcherTestSuite struct {
 
 func (suite *EventDispatcherTestSuite) SetupTest() {
   suite.eventDispatcher = NewEventDispatcher()
-  suite.handler = TestEventHandler{}
-  suite.handler2 = TestEventHandler{}
-  suite.handler3 = TestEventHandler{}
+  suite.handler = TestEventHandler{
+    ID: 1,
+  }
+  suite.handler2 = TestEventHandler{
+    ID: 2,
+  }
+  suite.handler3 = TestEventHandler{
+    ID: 3,
+  }
   suite.event = TestEvent{ Name: "test event", Payload: "test payload" }
   suite.event2 = TestEvent{ Name: "test event 2", Payload: "test payload 2" }
 }

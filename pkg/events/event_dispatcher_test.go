0f157fac03300c61cda5d116b338462203056be4
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-07-15 10:41:31 -0300

    test: unit tests for register method in dispatcher interface

--- pkg/events/event_dispatcher_test.go
+++ pkg/events/event_dispatcher_test.go
@@ -7,10 +7,31 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-func (suite *EventDispatcherTestSuite) TestEventDispatcher_Register() {
-  assert.True(suite.T(), true)
-}
-
 func TestSuite(t *testing.T) {
   suite.Run(t, new(EventDispatcherTestSuite))
+}
+
+func (suite *EventDispatcherTestSuite) TestEventDispatcher_Register() {
+  eventName := suite.event.GetName()
+
+  err := suite.eventDispatcher.Register(eventName, &suite.handler)
+  suite.Nil(err)
+  suite.Equal(1, len(suite.eventDispatcher.handlers[eventName]))
+  assert.Equal(suite.T(), &suite.handler, suite.eventDispatcher.handlers[eventName][0])
+
+  err = suite.eventDispatcher.Register(eventName, &suite.handler2)
+  suite.Nil(err)
+  suite.Equal(2, len(suite.eventDispatcher.handlers[eventName]))
+  assert.Equal(suite.T(), &suite.handler2, suite.eventDispatcher.handlers[eventName][1])
+}
+
+func (suite *EventDispatcherTestSuite) TestEventDispatcher_Register_WithSameHandler() {
+  eventName := suite.event.GetName()
+
+  suite.eventDispatcher.Register(eventName, &suite.handler)
+  err := suite.eventDispatcher.Register(eventName, &suite.handler)
+
+  suite.NotNil(err)
+  suite.Equal(ErrHandlerAlreadyRegistered, err)
+  suite.Equal(1, len(suite.eventDispatcher.handlers[eventName]))
 }
